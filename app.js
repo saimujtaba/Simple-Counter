f4f19aa9cbf3d9e62092b3b6198c93091dc4cd16
saimujtaba <saimujtaba99999@gmail.com>
2024-09-17 13:55:22 +0500

    minus button added

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var h1 = document.querySelector("h1")
-var counter = 0 ;
+var counter = 0;
 
 function plusbutton() {
-    counter += +1 ;
+    counter += +1;
     h1.innerHTML = counter;
 }
 
 function minusbutton() {
-    
+    counter = counter - 1;
+
+    if (counter <= 0) {
+        counter = 0;
+    }
+
+    h1.innerHTML = counter;
+
 }
